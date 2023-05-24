eecb9ce5803d5ed7b59dfeceac524d54c4ddb0d3
yunsg02 <127090156+yunsg02@users.noreply.github.com>
2023-05-24 19:10:59 +0200

    COLLISION

---
+++ P03_Final/Assets/Scripts/Particle System/FireColorChange.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireColorChange : MonoBehaviour
+{
+    public GameObject player;
+    public ParticleSystem fireParticles;
+    public Color closeColor;
+    public Color farColor;
+
+    private void Update()
+    {
+        // Calculate the distance between the player and the fire object
+        float distance = Vector3.Distance(player.transform.position, transform.position);
+
+        // Compare the distance with a threshold value
+        if (distance < 5f)
+        {
+            // Change the color of the particle system to closeColor
+            var mainModule = fireParticles.main;
+            mainModule.startColor = closeColor;
+        }
+        else
+        {
+            // Change the color of the particle system to farColor
+            var mainModule = fireParticles.main;
+            mainModule.startColor = farColor;
+        }
+    }
+}
